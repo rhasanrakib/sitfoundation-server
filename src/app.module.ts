27c9ec2ae47d5fccd74eaaa8a578ad7344acfc35
Rakib <rakibulh170@gmail.com>
2021-11-26 21:31:53 +0600

    env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntityModule } from './entity/entity.module';
@@ -13,10 +12,19 @@ import { SitfoundationModule } from './sitfoundation/sitfoundation.module';
     EntityModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        Object.assign(await getConnectionOptions(), {
-          autoLoadEntities: true,
-        }),
+      useFactory: async () => ({
+        type: 'mysql',
+        host: process.env.HOST,
+        port: 3306,
+        username: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true, // shouldn't be synchronized as true in production - otherwise you can lose production data.
+        extra: {
+          connectionLimit: 10,
+        },
+      }),
     }),
   ],
   controllers: [AppController],
